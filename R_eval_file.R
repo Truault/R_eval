b23c593c2d79ea9caf589886390ee5e51bfc187f
Truault <56683490+Truault@users.noreply.github.com>
2020-02-16 19:17:39 +0100

    Add files via upload

--- R_eval_file.R
+++ R_eval_file.R
@@ -1,36 +1,9 @@
 getwd()
-fichiers = dir()
-corpus_fichiers = lapply(fichiers, readLines)
+noms_fichiers = dir()#prend les files dans le wd
+corpus_fichiers = lapply(noms_fichiers, readLines)#transformation de tous les textes en liste
+corpus_fichiers_ve= unlist(corpus_fichiers)
+corpus_fichiers_liste = strsplit(corpus_fichiers_ve, " ")
+corpus_fichiers_vmots = unlist(corpus_fichiers_liste)
+corpus_fichiers_freq = summary(factor(corpus_fichiers_vmots))#fréquence des mots
+corpus_fichiers_freq[1:5]
 
-
-#transformation de tous les textes en liste
-
-#fonction pour calculer la fréquence des mots de chaque texte et mise dans un dataframe
-calcul_frequence_mot = function(mon_texte){
-  corpus_fichiers_ve= unlist(mon_texte)
-  #prend les files dans le wd  
-  corpus_fichiers_liste = strsplit(corpus_fichiers_ve, " ")
-  corpus_fichiers_vmots = unlist(corpus_fichiers_liste)
-  corpus_fichiers_freq = summary(factor(corpus_fichiers_vmots))#fréquence des mots
-  df_a = data.frame(col1=names(corpus_fichiers_freq[1:20]), col2 =corpus_fichiers_freq[1:20])
-  return(df_a)
-  
-}
-
-
-#alternative à la boucle for:
-for (i in 1:37) {print(calcul_frequence_mot(corpus_fichiers[i]))}
-
-
-
-
-
-
-#création des matrices de fréquence 20 pour chaque texte
-#vecteurs
-vn = resultat
-#fréq des 20 mots les plus fréquents
-matrice= matrix(v1, nrow = 37, ncol = 20)
-matrice
-
-#création du dendogramme permettant de visualiser la proximité des textes selon la fréquence de leur 40 et de leur 20 premiers mots. 
