62ca3e149c5b0560225634d5917e99fc776c9979
Truault <56683490+Truault@users.noreply.github.com>
2020-02-26 12:28:32 +0100

    Add files via upload

--- R_eval_file.R
+++ R_eval_file.R
@@ -1,9 +1,138 @@
+library(ggplot2)
 getwd()
-noms_fichiers = dir()#prend les files dans le wd
-corpus_fichiers = lapply(noms_fichiers, readLines)#transformation de tous les textes en liste
-corpus_fichiers_ve= unlist(corpus_fichiers)
-corpus_fichiers_liste = strsplit(corpus_fichiers_ve, " ")
-corpus_fichiers_vmots = unlist(corpus_fichiers_liste)
-corpus_fichiers_freq = summary(factor(corpus_fichiers_vmots))#fréquence des mots
-corpus_fichiers_freq[1:5]
+fichiers = dir()
+corpus_fichiers = lapply(fichiers, readLines)
+
+#transformation de tous les textes en liste
+
+#fonction pour calculer la fréquence des mots de chaque texte et mise dans un dataframe
+calcul_frequence_mot = function(mon_texte){
+  corpus_fichiers_ve1 = unlist(mon_texte)
+  corpus_fichiers_liste = strsplit(corpus_fichiers_ve1," ")
+  corpus_fichiers_vmots = unlist(corpus_fichiers_liste)
+  #on enlève un maximum de bruit
+  corpus_fichiers_vmots = corpus_fichiers_vmots[!((corpus_fichiers_vmots == " "))]
+  corpus_fichiers_vmots = corpus_fichiers_vmots[!((corpus_fichiers_vmots == ","))]
+  corpus_fichiers_vmots = corpus_fichiers_vmots[!((corpus_fichiers_vmots == "."))]
+  corpus_fichiers_vmots = corpus_fichiers_vmots[!((corpus_fichiers_vmots == ";"))]
+  corpus_fichiers_vmots = corpus_fichiers_vmots[!((corpus_fichiers_vmots == ";"))]
+  corpus_fichiers_vmots = corpus_fichiers_vmots[!((corpus_fichiers_vmots == "!"))]
+  corpus_fichiers_vmots = corpus_fichiers_vmots[!((corpus_fichiers_vmots == "?"))]
+  corpus_fichiers_vmots = corpus_fichiers_vmots[!((corpus_fichiers_vmots == "..."))]
+  corpus_fichiers_freq = summary(factor(corpus_fichiers_vmots))#fréquence des 
+  return(corpus_fichiers_freq[20:60])
+  
+}
+
+
+
+
+############################################################################################################
+############################################################################################################
+
+
+#analyse de la fréquence de 40 mots parmi les 200 mots les plus fréquents de chaque pièce 
+
+
+
+#creation de la matrice contenant les 40 mots les plus fréquents de chaque texte
+matrice_freq_test = matrix(1, nrow = 1, ncol = 1, byrow = TRUE)
+matrice_freq_test[1,1] = (calcul_frequence_mot(corpus_fichiers[1]))[1]
+matrice_freq_test
+#analyse de la fréquence de 40 mots parmi les 200 mots les plus fréquents de chaque pièce 
+matrice_freq = matrix(0, nrow = 37, ncol = 40, byrow = TRUE)
+for (j in 1:40){
+  for (i in 1:37){matrice_freq[i,j] = (calcul_frequence_mot(corpus_fichiers[i]))[j]
+  }
+}
+
+rownames(matrice_freq) = fichiers
+
+#graphiques
+#ACP
+
+freq_mat_acp = prcomp(matrice_freq[,1:40], scale = FALSE)
+biplot(freq_mat_acp)
+dfrd = data.frame(noms = fichiers, pc1 = freq_mat_acp$x[,1],
+                  pc2 = freq_mat_acp$x[,2])
+
+#affichage en nuage de points
+ggplot(data = dfrd, mapping = aes(x = pc1, y = pc2)) +
+  geom_point(aes(color= noms), size=5) +
+  scale_color_hue(h = c(0, 150))
+
+
+
+m = as.matrix(dfrd[,1:3])
+rownames(m) = fichiers
+d=dist(m)
+#classification automatique: affichage en cluster (groupés par auteurs)
+kmeans(m,4)
+dfrd$clust=factor(kmeans(m,4)$clust)#????
+
+
+
+#création du dendogramme permettant de visualiser la proximité des textes selon la fréquence de leur 40 et de leur 20 premiers mots.
+n = dist(matrice_freq)
+hc = hclust(d)
+plot(hc, xlab = " ", sub = " ", hang = -10)
+
+
+###############################################################################################################
+################################################################################################################
+
+#analyse de la fréquence des 20 mots les plus fréquents de chaque pièce 
+
+#creation de la matrice contenant les 20 mots les plus fréquents de chaque texte
+matrice_freq = matrix(0, nrow=37, ncol= 20, byrow = TRUE)
+for (j in 1:20) {print (j)
+  for (i in 1:37) {matrice_freq[i, j] = calcul_frequence_mot(corpus_fichiers[i], j)
+  }
+}
+
+rownames(matrice_freq) = fichiers
+
+
+
+#graphiques
+#ACP
+
+freq_mat_acp = prcomp(matrice_freq[,1:20], scale = TRUE)
+biplot(freq_mat_acp)
+dfrd = data.frame(noms = fichiers, pc1 = freq_mat_acp$x[,1],
+                  pc2 = freq_mat_acp$x[,2])
+
+
+#affichage en nuage de points
+ggplot(data = dfrd, mapping = aes(x = pc1, y = pc2)) +
+  geom_point(aes(color= noms), size=5) +
+  scale_color_hue(h = c(0, 150))
+
+
+#classification automatique: affichage en cluster (groupés par auteurs)
+m = as.matrix(dfrd[,2:3])
+rownames(m) = fichiers
+d=dist(m)
+kmeans(m,4)
+dfrd$clust=factor(kmeans(m,4)$clust)#????
+
+
+
+#création du dendogramme permettant de visualiser la proximité des textes selon la fréquence de leur 40 et de leur 20 premiers mots.
+hc = hclust(d)
+plot(hc, xlab = " ", sub = " ", hang = -10)
+
+#Tendances d'auteurs se dessinent: on remarque un regroupement  sur le graphique coloré
+#en fonction des auteurs bien que très distinct. En effet nous sommes partis de l'hypothèse 
+#que la signature d'un auteur se caractérise en partie par la même fréquence d'usage de mots. 
+#Cependant: le dendogramme ne signifie absolument rien. Comme l'ont fait JB Camps et F Cafiero,
+# il faudrait coupler cette analyse à une analyse stylométrique et morphosyntaxique pour 
+#identifier  chaque auteur. La fréquence de mots ne suffit pas. 
+
+
+
+
+
+
+
 
